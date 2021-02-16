7428aa1f79b5c56d5c89d06583c8e62d41eb12af
mwb <631714470@qq.com>
2021-02-16 12:26:52 +0800

    数组生成器和时间记录

---
+++ src/mkw/linearSearch/ArrayGenerator.java
@@ -0,0 +1,16 @@
+package mkw.linearSearch;
+
+/**
+ * 数组生成器
+ */
+public class ArrayGenerator {
+    private ArrayGenerator(){}
+
+    public static Integer[] generateOrderedArray(int n){
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i<n; i++){
+            arr[i] = i;
+        }
+        return arr;
+    }
+}
--- src/mkw/linearSearch/LinearSearch.java
+++ src/mkw/linearSearch/LinearSearch.java
@@ -9,17 +9,19 @@ public class LinearSearch {
     private LinearSearch(){}
 
     public static void main(String[] args) {
-
-        Student[] stArr = {new Student("zhangsan"),
-                new Student("lisi"),
-                new Student("wanger")};
-
-        System.out.println(LinearSearch.find(stArr,new Student("lisi")));
+        int n = 10000000;
+        Integer[] arr =  ArrayGenerator.generateOrderedArray(n);
+        long startTime = System.nanoTime();
+        LinearSearch.find(arr,10000000);
+        long endTime = System.nanoTime();
+        double time = (endTime - startTime) / 1000000000.0;
+        System.out.println(time);
     }
 
     /**
      * 入参一个数组和需要查找的元素
      * 返回目标所在的索引
+     * 自定义数组的类型，需要自己实现比对芳芳
      * @param intArr
      * @param num
      * @return
@@ -32,4 +34,5 @@ public class LinearSearch {
         }
         return -1;
     }
+
 }
